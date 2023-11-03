592b9ff44ed784e8231cb6a0dac3968246a94476
AleksandrSoiko <farshchef777@gmail.com>
2023-11-03 20:51:24 +0000

    fix filter

--- src/app/searchPage/page.js
+++ src/app/searchPage/page.js
@@ -1,25 +1,20 @@
-import RentBuySelect from '../components/RentBuySelect'
-import LatestOffers from '../components/LatestOffers'
-import Filter from './components/Filter'
-
+import RentBuySelect from "../components/RentBuySelect";
+import LatestOffers from "../components/LatestOffers";
+import Filter from "./components/filter";
 
 const fetchResultsByHouse = () => {
-    return (
-        <section
-        className="flex flex-col items-center mx-4 mt-[3.75rem]  max-md:py-[1.44rem] md:pb-16"
-    >
-            <RentBuySelect />
-            <Filter />
-            <div className="mt-12 mb-6 mx-4 md:mb-16 lg:mb-20 flex flex-col items-center">
-                <p
-                    className="text-center font-Comfortaa text-2xl font-normal leading-[1.8rem] md:text-[3.125rem] md:leading-[3.75rem]"
-                >
-                    Results
-                </p>
-                <LatestOffers />
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="flex flex-col items-center mx-4 mt-[3.75rem]  max-md:py-[1.44rem] md:pb-16">
+      <RentBuySelect />
+      <Filter />
+      <div className="mt-12 mb-6 mx-4 md:mb-16 lg:mb-20 flex flex-col items-center">
+        <p className="text-center font-Comfortaa text-2xl font-normal leading-[1.8rem] md:text-[3.125rem] md:leading-[3.75rem]">
+          Results
+        </p>
+        <LatestOffers />
+      </div>
+    </section>
+  );
+};
 
-export default fetchResultsByHouse
+export default fetchResultsByHouse;
