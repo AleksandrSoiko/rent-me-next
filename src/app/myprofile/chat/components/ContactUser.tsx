9e08051b91e71fe90e66d96a0159a0556c5428b4
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-11-30 15:53:30 +0200

    chat

---
+++ src/app/myprofile/chat/components/ContactUser.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+export const ContactUser = () => {
+  return (
+    <div className="bg-blue1 rounded-[0.5rem] px-[1rem] py-[0.62rem] flex justify-between">
+      <div className="flex gap-[0.62rem]">
+        <Image
+          src="/Latestoffers/Avatar.png"
+          width="44"
+          height="44"
+          alt="avatar"
+          className="rounded-[100%]"
+        />
+        <div className="flex flex-col justify-between">
+          <p className="text-sm font-Manrope height-[1.05rem]">Ben Swam</p>
+          <p className="text-[0.625rem] font-Manrope height-[0.9rem] text-grayText">
+            Hello
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col justify-between">
+        <span className="text-[0.75rem] font-Manrope height-[0.9rem] text-grayText">
+          11:23
+        </span>
+        <span className="bg-borderBlue w-[1rem] h-[1rem] text-xs font-medium font-Inter text-center rounded-[100%] ">
+          1
+        </span>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/myprofile/chat/components/Contacts.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { ContactUser } from "./ContactUser";
+
+export const Contacts = () => {
+  return (
+    <div className="p-[1rem] border-[1px] border-blue1 rounded-[0.5rem]">
+      <div className="flex justify-between">
+        <p className="font-semibold text-base height-[1.2rem] font-Manrope">
+          Contacts
+        </p>
+        <p>+</p>
+      </div>
+      <div className="relative">
+        <input
+          type="text"
+          placeholder="Search"
+          className="pl-[2.6rem] py-[0.8rem] pr-[1rem] border-[1px] border-blue1 rounded-[0.5rem] my-[1rem]"
+        />
+        <Image
+          src="/RentBuySelect/searchIcon.svg"
+          width="16"
+          height="19"
+          alt="LocationSvg"
+          className="absolute top-[35px] left-[15px]"
+        />
+      </div>
+      <ul>
+        <li>
+          <ContactUser />
+        </li>
+      </ul>
+    </div>
+  );
+};
