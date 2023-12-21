6c6b665f018b03246e5c04c5398958269e13c2d8
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-12-21 14:44:12 +0200

    sl

--- src/app/account/agent/components/YourProperties.tsx
+++ src/app/account/agent/components/YourProperties.tsx
@@ -4,6 +4,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import FullScreenImage from "../../../apartament/[slug]/components/openImgModal";
+import { btnHoverOrange, btnHoverOrangeReverse } from "../../../page";
 const image: string[] = [
   "/LatestOffers/sliderTest/qwe.webp",
   "/LatestOffers/sliderTest/qwer.jpg",
@@ -28,7 +29,7 @@ const YourProperties = () => {
   return (
     <li>
       <div className="lg:flex gap-5 my-8">
-        <div className="w-[580px] mb-[32px]">
+        <div className="w-[580px] mb-[80px]">
           <Slider {...settings} className="custom-slider">
             {image.map((image, index) => (
               <div key={index}>
@@ -57,7 +58,7 @@ const YourProperties = () => {
               </p>
             </div>
           </div>
-          <div className="">
+          <div>
             <div className="flex flex-col text-[#000] lg:w-[36.25rem] font-Comfortaa text-xl md:text-2xl md:font-semibold md:leading-[1.8rem] font-medium leading-[1.5rem]">
               <p className="md:mb-5 font-semibold">£876 pcm</p>
               <p className="text-xl md:mb-6">Property description</p>
@@ -65,17 +66,21 @@ const YourProperties = () => {
                 This modern two bedroom property will come fully furnished and
                 is suitable for students. The property comprises of a large hall
                 with plenty of storage cupboards. Off the hall the modern
-                kitchen has... <span className="underline">more</span>
+                kitchen has... <button className="underline">more</button>
               </p>
             </div>
           </div>
         </div>
       </div>
       <div className="flex gap-4 max-md:justify-center">
-        <button className="w-[9.75rem] lg:w-[11.25rem] px-2 py-[0.625rem] text-[#fff] whitespace-nowrap text-center bg-orange text-ellipsis font-Comfortaa text-sm font-semibold w-[8.75rem] rounded-[0.625rem]">
+        <button
+          className={`${btnHoverOrange} w-[9.75rem] lg:w-[11.25rem] px-2 py-[0.625rem] text-[#fff] whitespace-nowrap text-center bg-orange text-ellipsis font-Comfortaa text-sm font-semibold w-[8.75rem] rounded-[0.625rem]`}
+        >
           Edit
         </button>
-        <button className="w-[9.75rem] lg:w-[11.25rem] px-8 py-[0.625rem] lg:py-4 whitespace-nowrap text-center border-[1px] border-orange text-ellipsis font-Comfortaa text-sm font-semibold w-[8.75rem] rounded-[0.625rem]">
+        <button
+          className={`${btnHoverOrangeReverse} w-[9.75rem] lg:w-[11.25rem] px-8 py-[0.625rem] lg:py-4 whitespace-nowrap text-center border-[1px] border-orange text-ellipsis font-Comfortaa text-sm font-semibold w-[8.75rem] rounded-[0.625rem]`}
+        >
           Delete
         </button>
       </div>
