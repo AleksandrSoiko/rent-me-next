305c7e36fe27ab2dfd29854e3d960a84df954e51
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2023-12-28 17:01:07 +0200

    import font

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
-
       <body>
         <main className="min-w-min bg-white ">
           <Header />
