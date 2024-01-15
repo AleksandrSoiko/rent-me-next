18e82abbdeffa7cf57ee4fcea8def0d8962eed0e
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2024-01-15 17:02:13 +0200

    s

---
+++ src/config/api.confog.ts
@@ -0,0 +1,3 @@
+export const API_URL = `${process.env.APP_URL}/api`
+
+export const getAll = (string: string) => `/apartament${string}`
---
+++ src/types/apartament.types.ts
@@ -0,0 +1,10 @@
+export interface Apartament {
+	_id: string
+	title: string
+	pictures: string[]
+	place: string
+	price: number
+	description: string
+	genres: string[]
+	agent: Object[]
+}
