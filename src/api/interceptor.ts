c280cd9e37c74c3bc0774506dc315926c47236ce
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2024-01-19 12:46:02 +0200

    asd

--- src/api/interceptor.ts
+++ src/api/interceptor.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
-import { API_URL } from 'config/api.config'
+import { API_URL, NEXTAUTH_API } from 'config/api.config'
 import { authOptions } from 'lib/auth'
 import { getServerSession } from 'next-auth'
 
 const axiosApi = axios.create({
-	baseURL: API_URL,
+	baseURL: 'https://nodejs-hw-goit-03-restapi.onrender.com/',
 	timeout: 1000,
 	headers: { 'Content-Type': 'application/json' },
 })
@@ -25,7 +25,7 @@ axiosApi.interceptors.request.use(async (config) => {
 export default axiosApi
 
 export const axiosClassic = axios.create({
-	baseURL: API_URL,
+	baseURL: 'https://nodejs-hw-goit-03-restapi.onrender.com/',
 	headers: {
 		'Content-Type': 'application/json',
 	},
