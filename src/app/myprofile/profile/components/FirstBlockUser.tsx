86d9afbdca6dbef5e3ff95355c4205f394bf39ab
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2024-01-25 10:21:48 +0200

    end profile back

--- src/app/myprofile/profile/components/FirstBlockUser.tsx
+++ src/app/myprofile/profile/components/FirstBlockUser.tsx
@@ -6,10 +6,21 @@ import { IUser } from 'types/user.types'
 import useAxiosPost from 'hooks/useAxios'
 import 'toastr/build/toastr.css'
 import toastr from 'toastr'
-import axios from 'axios'
+import { useEffect } from 'react'
 
 export const FirstBlockUser: React.FC<{ profile: IUser }> = ({ profile }) => {
-	const { data, loading, error, fetchAxios } = useAxiosPost()
+	const {
+		data: data1,
+		loading: loading1,
+		error: error1,
+		fetchAxios: fetchAxios1,
+	} = useAxiosPost()
+	const {
+		data: data2,
+		loading: loading2,
+		error: error2,
+		fetchAxios: fetchAxios2,
+	} = useAxiosPost()
 
 	const handleFileChange = async (
 		event: React.ChangeEvent<HTMLInputElement>
@@ -19,9 +30,8 @@ export const FirstBlockUser: React.FC<{ profile: IUser }> = ({ profile }) => {
 		if (file) {
 			const formData = new FormData()
 			formData.append('photo', file)
-			// axios.post('http://localhost:4000/api/files/', formData).then(res=> console.log(res))
 			try {
-				await fetchAxios({
+				await fetchAxios1({
 					url: '/files',
 					method: 'POST',
 					body: formData,
@@ -31,6 +41,33 @@ export const FirstBlockUser: React.FC<{ profile: IUser }> = ({ profile }) => {
 			}
 		}
 	}
+
+	const deletePhoto = async () => {
+		try {
+			await fetchAxios2({
+				url: '/files',
+				method: 'DELETE',
+				body: {},
+			})
+		} catch (error) {
+			console.error('Error uploading photo', error)
+		}
+	}
+
+	useEffect(() => {
+		if (error1) {
+			toastr.error(error1)
+		} else if (data1) {
+			toastr.success('You successfully change photo')
+			window.location.reload()
+		}
+		if (error2) {
+			toastr.error(error2)
+		} else if (data2) {
+			toastr.success('You successfully delete photo')
+			window.location.reload()
+		}
+	}, [error1, data1, error2, data2])
 
 	return (
 		<div className="p-[1.5rem] w-[17.4rem] max-lg:mx-[auto]">
@@ -49,7 +86,10 @@ export const FirstBlockUser: React.FC<{ profile: IUser }> = ({ profile }) => {
 					Add foto+
 					<input id="addphoto" type="file" onChange={handleFileChange} />
 				</label>
-				<button className="text-xs font-medium height-[1.125rem] font-Inter hover:text-orange">
+				<button
+					onClick={deletePhoto}
+					className="text-xs font-medium height-[1.125rem] font-Inter hover:text-orange"
+				>
 					Delete photo
 				</button>
 			</div>
--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
-// export const API_URL = `${process.env.NEXTAUTH_API}/api`
-export const API_URL = 'http://localhost:4000/api'
+export const API_URL = `${process.env.NEXTAUTH_API}/api`
+// export const API_URL = 'http://localhost:4000/api'
 
 export const getAll = (string: string) => `/apartament${string}`
 export const getUserProfile = (string: string) => `/users${string}`
