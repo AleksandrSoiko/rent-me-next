99e3bb61ec2463d0844f5f13469c387801136cb8
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2024-01-25 10:48:06 +0200

    fix api

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
-export const API_URL = `${process.env.NEXTAUTH_API}/api`
+export const API_URL = `https://nodejs-hw-goit-03-restapi.onrender.com/api`
 // export const API_URL = 'http://localhost:4000/api'
 
 export const getAll = (string: string) => `/apartament${string}`
