bf7fe7227ec2a0aec20efd4c5ec0d38196ee47f2
Serhii <110424622+StaloneSerhii@users.noreply.github.com>
2024-02-01 15:20:26 +0200

    api

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
-// export const API_URL = `https://nodejs-hw-goit-03-restapi.onrender.com/api`
-export const API_URL = 'http://localhost:4000/api'
+export const API_URL = `https://nodejs-hw-goit-03-restapi.onrender.com/api`
+// export const API_URL = 'http://localhost:4000/api'
 //
 export const getAll = (string: string) => `/apartament${string}`
 export const getUserProfile = (string: string) => `/users${string}`
